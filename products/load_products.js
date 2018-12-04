ebdae17054b26d4a2df19a62f9c2548e1ff50e67
SACHIN SHARMA <sachinsharma3191@gmail.com>
2018-12-04 09:03:09 +0000

    Integrate code changes of hema with my code

--- products/load_products.js
+++ products/load_products.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    let addToCartButtonCounter= 0;
     let products = [];
                 //Load products from db
                 $.ajax({
@@ -28,7 +29,7 @@ $(document).ready(function() {
             let productName = results["ProductName"];
             let productPrice = results["Price"];
             let imageName = results["ProductImage"];
-            let imagDiv = '<img class="img-fluid" src="productimages/' + imageName.toLowerCase()  + '" alt="">'
+            let imagDiv = '<img class="img-fluid" src="sachin_images/' + imageName.toLowerCase()  + '" alt="">'
             let addToCartFunction = "addToCart(" + productId +")";
             let cartIcon = '<div class="p_icon"><a href="#"><i class="lnr lnr-heart"></i></a><button name="addToCart" type="button" id="addToCart" value='+ productId + ' class="addToCart btn"><i class="lnr lnr-cart"></i></button></div></div>'
             let productNameDiv  = '<a href="#"><h4>' + productName + '</h4></a>';
@@ -39,20 +40,31 @@ $(document).ready(function() {
                      
         }
     }
-    
     //Handle Function for Add To Cart Functionality
     $('body').on("click", "button[name=addToCart]", function (){
+        addToCartButtonCounter++;
         let productId = $(this).attr("value");
-        let productDetail = products.filter(product => product.ProductID === productId)[0];
-        if(!productDetail.hasOwnProperty("productCount")){
-            productDetail.productCount = 1;
-        }
-        else if(productDetail.hasOwnProperty("productCount")){
-            let previousProductCount = productDetail.productCount;
-            productDetail.productCount = previousProductCount + 1;
+        let detailedProduct = null;
+        $.ajax({
+            url: "products/fetch_product.php",
+            method:"POST",
+            data: {productId : productId},
+            success: (data) => {
+                if(data.includes("No Product Found")){
+                    alert("Unable to add to to cart");
+                }
+                else  if(Object.keys(JSON.parse(data)).length > 0 ){
+                    detailedProduct = JSON.parse(data)[0];   
+                }
+            },
+            async: false 
+         });
+         
+         if(!detailedProduct.hasOwnProperty("productCount")){
+            detailedProduct.productCount = addToCartButtonCounter;
         }
         let data = {
-            product : JSON.stringify(productDetail)
+            product: JSON.stringify(detailedProduct)
         }
         $.ajax({
             url: "cart/add_to_cart.php",
@@ -68,6 +80,5 @@ $(document).ready(function() {
             },
             async: false 
          }); 
-      
     });
 });
