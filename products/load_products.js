9646c39d89c1d35bfc2777149f5a732c26089cac
SACHIN SHARMA <sachinsharma3191@gmail.com>
2018-12-05 18:50:10 -0800

    Update load_products.js

--- products/load_products.js
+++ products/load_products.js
@@ -1,32 +1,41 @@
 $(document).ready(function() {
-    let addToCartButtonCounter= 0;
     
+    let products = [];
+                //Load products from db
+                $.ajax({
+                   url: "products/load_products.php",
+                   method:"GET",
+                   success: (data) => {
+                       if(data.includes("No Products to Load")){
+                           alert("No Products to load");
+                       }
+                       if(Object.keys(data).length > 0){
+                          products = JSON.parse(data);
+                        }
+                   },
+                   async: false 
+                 }); 
+
+
     //Handle Function for Add To Cart Functionality
     $('body').on("click", "button[name=addToCart]", function (){
-        addToCartButtonCounter++;
+
         let productId = $(this).attr("value");
-        let detailedProduct = null;
-        $.ajax({
-            url: "products/fetch_product.php",
-            method:"POST",
-            data: {productId : productId},
-            success: (data) => {
-                if(data.includes("No Product Found")){
-                    alert("Unable to add to to cart");
-                }
-                else  if(Object.keys(JSON.parse(data)).length > 0 ){
-                    detailedProduct = JSON.parse(data)[0];   
-                }
-            },
-            async: false 
-         });
-         
-         if(!detailedProduct.hasOwnProperty("productCount")){
-            detailedProduct.productCount = addToCartButtonCounter;
+        let productDetail = products.filter(product => product.ProductID === productId)[0];
+
+        if(!productDetail.hasOwnProperty("productCount")){
+            productDetail.productCount = 1;
         }
+        else if(productDetail.hasOwnProperty("productCount")){
+            let previousProductCount = productDetail.productCount;
+            productDetail.productCount = previousProductCount + 1;
+        }
+
         let data = {
-            product: JSON.stringify(detailedProduct)
+            product: JSON.stringify(productDetail)
         }
+
+        console.log(data);
         $.ajax({
             url: "cart.php",
             method:"POST",
